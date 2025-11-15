730ec1ade9f0aaae1c0eeab935eced41c9267109
yisel35 <kimberlynyisel35@gmail.com>
2025-11-15 14:59:03 -0300

    version0.83

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,34 +5,45 @@ import Navbar from 'react-bootstrap/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css"; 
 
 export default function MyImpactfulNavbar() {
+  
   return (
-    <Navbar className="navbar-glass" expand="lg" sticky="top"> 
-      <Container>
-        
-     
-        <Navbar.Brand 
-          className="logo" 
-          style={{ fontSize: '2.5rem' }} 
-        >
-        
-         <h1>🚀Kimberlyn M. | Mi viaje creativo🌍</h1>
-        </Navbar.Brand>
-        <Nav className="nav-links" activeKey="#intro">
-          
-          <Nav.Item>
-            <Nav.Link href="#intro">Inicio</Nav.Link> 
-          </Nav.Item>
+    // 'expand="lg"' indica que el menú colapsará (se hará hamburguesa)
+    // en tamaños de pantalla menores a 'large' (992px)
+<Navbar className="navbar-glass" expand="lg" sticky="top"> 
+<Container>
 
-          <Nav.Item>
-            <Nav.Link href="#formation">Formación</Nav.Link>
-          </Nav.Item>
-          
-          <Nav.Item>
-            <Nav.Link href="#projects">Proyectos</Nav.Link>
-          </Nav.Item>
-          
-        </Nav>
-      </Container>
-    </Navbar>
-  );
+ <Navbar.Brand 
+ className="logo" 
+ >
+<h1>🚀Kimberlyn M. | Mi viaje creativo🌍</h1>
+ </Navbar.Brand>
+        
+        {/* 2. BOTÓN DE HAMBURGUESA (Navbar.Toggle) */}
+        {/* Este botón es visible solo en pantallas pequeñas (debido a expand="lg") */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        {/* 3. CONTENIDO COLAPSABLE (Navbar.Collapse) */}
+        {/* Este contenedor es lo que se muestra/oculta al hacer clic en el Toggle */}
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            
+            <Nav className="nav-links" activeKey="#intro">
+                <Nav.Item>
+                    {/* Asegúrate que las clases de tu CSS (como .nav-link)
+                        se apliquen a los Nav.Link si es necesario */}
+                    <Nav.Link href="#intro">Inicio</Nav.Link> 
+                </Nav.Item>
+
+                <Nav.Item>
+                    <Nav.Link href="#formation">Formación</Nav.Link>
+                </Nav.Item>
+                
+                <Nav.Item>
+                    <Nav.Link href="#projects">Proyectos</Nav.Link>
+                </Nav.Item>
+            </Nav>
+
+        </Navbar.Collapse>
+ </Container>
+ </Navbar>
+ );
 }
