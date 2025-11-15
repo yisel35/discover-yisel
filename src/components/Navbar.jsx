e15f26cc579e128b13fc25c37e3798fd9d720ea9
yisel35 <kimberlynyisel35@gmail.com>
2025-11-15 14:14:50 -0300

    veriosn0.78

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,12 +18,7 @@ export default function MyImpactfulNavbar() {
         
          <h1>🚀Kimberlyn M. | Mi viaje creativo🌍</h1>
         </Navbar.Brand>
-        <Nav className="nav-links ms-auto" activeKey="#home">
-          
-          <Nav.Item>
-            <Nav.Link href="#intro">Inicio</Nav.Link> 
-          </Nav.Item>
-
+        <Nav className="nav-links ms-auto" activeKey="#intro">
           <Nav.Item>
             <Nav.Link href="#formation">Formación</Nav.Link>
           </Nav.Item>
